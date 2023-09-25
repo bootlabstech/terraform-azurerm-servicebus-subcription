a2d82152901928efe8d2c2bbd8117df6d6fc6a6d
lalita2407 <lalitalallu02@gmail.com>
2023-09-25 21:31:52 +0530

    added  enable_batched_operations variable

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ resource "azurerm_servicebus_subscription" "example" {
   name               = each.value.name
   topic_id           = var.topic_id
   max_delivery_count = each.value.max_delivery_count
+  dead_lettering_on_filter_evaluation_error = each.value.dead_lettering_on_filter_evaluation_error
+  enable_batched_operations = each.value.enable_batched_operations
 }
 
 
